145ca4bc437a713ef41dc15106066a7d51c8d063
Denison Percey <denisonipercey@outlook.com>
2022-01-19 10:19:50 -0400

    Finished player class, finished program

--- HealthSystemV2/Player.cs
+++ HealthSystemV2/Player.cs
@@ -9,29 +9,103 @@ namespace HealthSystemV2
     class Player
     {
 
-        public int currentHealth = 100;
+        public int currentHealth;
+        //public int currentShield;
+        public int currentLives;
 
-        public void TakeDamage(int damageTaken)
-        {
-            currentHealth = currentHealth - damageTaken;
-
-        }
         public Player()
         {
             currentHealth = 100;
+            //currentShield = 100;
+            currentLives = 3;
         }
+
+        public void TakeDamage(int damageTaken)
+        {
+            Console.WriteLine("Player takes " + damageTaken + " damage");
+
+            //if (damageTaken >= 0)
+            //{
+            //    currentShield = currentShield - damageTaken;
+
+            //    if (currentShield <= 0)
+            //    {
+            //        currentHealth = currentHealth - currentShield;
+            //    }
+            //    if (currentShield <= 0)
+            //    {
+            //        currentShield = 0;
+            //        currentHealth = currentHealth - damageTaken;
+            //    }
+            //    if (currentHealth <= 0)
+            //    {
+            //        currentLives = currentLives - 1;
+            //        Respawn();
+            //        if (currentLives == 0)
+            //        {
+            //            GameOver();
+            //        }
+            //    }
+            //}
+
+            currentHealth = currentHealth - damageTaken;
+            if (currentHealth <= 0)
+            {
+                currentLives = currentLives - 1;
+                Respawn();
+                if (currentLives == 0)
+                {
+                    GameOver();
+                }
+            }
+
+
+            ShowHUD();
+            Console.ReadKey(true);
+        }
+
+        public void GameOver()
+        {
+            Console.WriteLine("GAME OVER");
+            Console.ReadKey(true);
+            ResetGame();
+        }
+
+        public void ResetGame()
+        {
+            Console.WriteLine("Restarting...");
+            Console.ReadKey(true);
+            currentHealth = 100;
+            //currentShield = 100;
+            currentLives = 3;
+            
+        }
+
+        public void Respawn()
+        {
+            Console.WriteLine("You Died");
+            Console.WriteLine("Respawn");
+            Console.ReadKey(true);
+            currentHealth = 100;
+            //currentShield = 100;
+        }
+            
         public void Heal(int healthHealed)
         {
             currentHealth = currentHealth + healthHealed;
+            Console.WriteLine("Player heals " + healthHealed + " health");
+            ShowHUD();
+            Console.ReadKey(true);
         }
-        public void ShowStats()
+        public void ShowHUD()
         {
             Console.WriteLine();
             Console.WriteLine("--------------HUD--------------");
-            Console.WriteLine("Player health: " + currentHealth);
+            Console.WriteLine("Player health: " + currentHealth + "%");
+            //Console.WriteLine("Player shield: " + currentShield + "%");
+            Console.WriteLine("Player lives: " + currentLives);
             Console.WriteLine("-------------------------------");
             Console.WriteLine();
         }
-
     }
 }
--- HealthSystemV2/Program.cs
+++ HealthSystemV2/Program.cs
@@ -8,17 +8,22 @@ namespace HealthSystemV2
 {
     class Program
     {
-        static Player player1 = new Player;
+        static Player player1 = new Player();
 
         static void Main(string[] args)
         {
             Console.WriteLine("Health System V.2");
             Console.WriteLine("Incorporating OOP");
+            Console.ReadKey(true);
+
+            player1.ShowHUD();
             Console.ReadKey();
 
-            player1.ShowStats();
-
-
+            player1.TakeDamage(50);
+            player1.Heal(25);
+            player1.TakeDamage(100);
+            player1.TakeDamage(150);
+            player1.TakeDamage(150);
 
             Console.ReadKey(true);
         }
